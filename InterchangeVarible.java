4cca5d4316f3c98eff05ff92695cc8d898c61dee
KumariPragati19 <122395348+KumariPragati19@users.noreply.github.com>
2023-01-19 09:17:41 +0530

    Add files via upload

---
+++ InterchangeVarible.java
@@ -0,0 +1,16 @@
+//interchange or replace two variable withou using third variable...
+import java.util.*;
+
+public class InterchangeVarible {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Inter two number want to interchange: ");
+        int a=sc.nextInt();
+        int b= sc.nextInt();
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ LetterCount.java
@@ -0,0 +1,26 @@
+//Take a String and find the count of each letter in the String(without repeat means case insesitive)
+import java.util.*;
+public class LetterCount {
+   public static void main(String[] args){
+    String a;
+    String b;
+    int count[] =new int[256];
+
+    Scanner sc= new Scanner(System.in);
+
+    System.out.print("Enter the string : ");
+
+    a = sc.nextLine();
+    b=a.toLowerCase();
+
+    for(int i=0;i<b.length();i++){
+        count[(int)b.charAt(i)]++;
+    }
+    for(int i=0;i<256;i++){
+        if(count[i]!=0){
+            System.out.println((char)i+"="+count[i]);
+        }
+    }
+
+   } 
+}
